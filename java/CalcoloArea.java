486010fd57ee81251d82961449ca11ad6f139599
Massimiliano Atzori <massimiliano@develer.com>
2020-02-22 11:54:07 +0100

    Modifico testo

--- java/CalcoloArea.java
+++ java/CalcoloArea.java
@@ -11,10 +11,10 @@ public class CalcoloArea {
 		int base = 0;
 		int altezza = 0;
 
-		System.out.println("Base del triangolo:");
+		System.out.println("Base del mio triangolo:");
 		base = readInput.nextInt();
      
-		System.out.println("Altezza del triangolo:");
+		System.out.println("Altezza del mio triangolo:");
         altezza = readInput.nextInt();
         readInput.close();
  
